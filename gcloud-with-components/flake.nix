b4a8af78842055938fe518c79dc1770c1eebe667
Tom Taylor <tom@tomtaylor.co.uk>
2026-02-06 12:50:18 +0000

    Add flake for gcloud with components we use

---
+++ gcloud-with-components/flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages = builtins.mapAttrs (system: pkgs: {
+      default = pkgs.google-cloud-sdk.withExtraComponents( with pkgs.google-cloud-sdk.components; [
+        gke-gcloud-auth-plugin
+        pubsub-emulator
+      ]);
+    }) nixpkgs.legacyPackages;
+  };
+}
